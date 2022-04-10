d6d13eae028d220c5c35981b162587da768ac113
MANtor123 <mantombiratego@gmail.com>
2022-04-10 05:05:09 +0200

    Added builder on the domain classes

--- src/main/java/za/ac/cput/entity/Contract.java
+++ src/main/java/za/ac/cput/entity/Contract.java
@@ -7,12 +7,129 @@ package za.ac.cput.entity;
 
 public class Contract {
 
-    private date signedDate;
-    private date expDate;
+    private LocalDate signedDate;
+    private LocalDate expDate;
     private locationId signedLocation;
-    private witness PersonalDetails;
+    private witness personalDetails;
     private int duration;
 
+    private Contract(Builder builder){
+        this.signedDate = builder.signedDate;
+        this.expDate = builder.expDate;
+        this.signedLocation = builder.signedLocation;
+        this.personalDetails = builder.personalDetails;
+        this.duration = builder.duration;
+    }
+
+    public LocalDate getSignedDate() {
+        return signedDate;
+    }
+
+    public void setSignedDate(LocalDate signedDate) {
+        this.signedDate = signedDate;
+    }
 
 
+
+    public locationId getSignedLocation() {
+        return signedLocation;
+    }
+
+    public void setSignedLocation(locationId signedLocation) {
+        this.signedLocation = signedLocation;
+    }
+
+
+
+    public witness getPersonalDetails() {
+        return personalDetails;
+    }
+
+    public void setPersonalDetails(witness personalDetails) {
+        this.personalDetails = personalDetails;
+    }
+
+    public LocalDate getExpDate() {
+        return expDate;
+    }
+
+    public void setExpDate(LocalDate expDate) {
+        this.expDate = expDate;
+    }
+
+
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Contract{" +
+                "signedDate=" + signedDate +
+                ", expDate=" + expDate +
+                ", signedLocation=" + signedLocation +
+                ", personalDetails=" + personalDetails +
+                ", duration=" + duration +
+                '}';
+    }
+
+
+    public static class Builder {
+
+        private LocalDate signedDate;
+        private LocalDate expDate;
+        private locationId signedLocation;
+        private witness personalDetails;
+        private int duration;
+
+
+
+        public Builder setSignedDate(LocalDate signedDate) {
+            this.signedDate = signedDate;
+            return this;
+        }
+
+        public Builder setExpDate(LocalDate expDate) {
+            this.expDate = expDate;
+            return this;
+        }
+
+        public Builder setSignedLocation(locationId signedLocation) {
+            this.signedLocation = signedLocation;
+            return this;
+        }
+
+        public Builder setPersonalDetails(witness personalDetails) {
+            this.personalDetails = personalDetails;
+            return this;
+        }
+
+        public Builder setDuration(int duration) {
+            this.duration = duration;
+            return this;
+        }
+
+        public Builder copy(Contract contract){
+
+            this.signedDate = contract.signedDate;
+            this.expDate = contract.expDate;
+            this.signedLocation = contract.signedLocation;
+            this.personalDetails = contract.personalDetails;
+            this.duration = contract.duration
+
+            return this;
+        }
+
+        public Contract build(){
+            return new Contract(this);
+        }
+
+
+
+    }
 }
--- src/main/java/za/ac/cput/entity/PlayerContract.java
+++ src/main/java/za/ac/cput/entity/PlayerContract.java
@@ -12,5 +12,102 @@ public class PlayerContract {
     private int matchesPlayed;
     private Contract contract;
 
+    private PlayerContract(Builder builder) {
+        this.contractid = builder.contractid;
+        this.goalTarget = builder.goalTarget;
+        this.matchesPlayed = builder.matchesPlayed;
+        this.contract = builder.contract;
+    }
 
+    public int getContractid() {
+        return contractid;
+    }
+
+    public void setContractid(int contractidate) {
+        this.contractid = contractid;
+    }
+
+
+    public int getGoalTarget() {
+        return goalTarget;
+    }
+
+    public void setGoalTarget(int goalTarget) {
+        this.goalTarget = goalTarget;
+    }
+
+
+    public int getMatchesPlayed() {
+        return matchesPlayed;
+    }
+
+    public void setMatchesPlayed(int MatchesPlayed) {
+        this.matchesPlayed = matchesPlayed;
+    }
+
+    public void setContract(Contract contract) {
+        this.contract = contract;
+    }
+
+    public Contract getContract() {
+        return contract;
+    }
+
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "PlayerContract{" +
+                "contractid=" + contractid +
+                ", goalTarget=" + goalTarget +
+                ", matchesPlayed=" + matchesPlayed +
+                ", contract=" + contract +
+                '}';
+    }
+
+    public static class Builder {
+
+        private int contractid;
+        private int goalTarget;
+        private int matchesPlayed;
+        private Contract contract;
+
+
+        public Builder setContractid(int contractid) {
+            this.contractid = contractid;
+            return this;
+        }
+
+        public Builder setGoalTarget(int goalTarget) {
+            this.goalTarget = goalTarget;
+            return this;
+        }
+
+        public Builder setMatchesPlayed(int matchesPlayed) {
+            this.matchesPlayed = matchesPlayed;
+            return this;
+        }
+
+        public Builder setContract(Contract contract) {
+            this.contract = contract;
+            return this;
+        }
+
+
+        public Builder copy(PlayerContract playerContract) {
+
+            this.contractid = playerContract.contractid;
+            this.goalTarget = playerContract.goalTarget;
+            this.matchesPlayed = playerContract.matchesPlayed;
+            this.contract = playerContract.contract;
+
+
+            return this;
+        }
+
+        public PlayerContract build() {
+            return new PlayerContract(this);
+        }
+
+
+    }
 }
