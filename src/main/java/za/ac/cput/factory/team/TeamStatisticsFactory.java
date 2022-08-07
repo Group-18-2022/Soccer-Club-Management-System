55fd753724ca833280ec00b66c2794131e440771
miketyo <mikesomelezotyolani41@gmail.com>
2022-08-07 22:07:27 +0200

    Refactored TeamStatisticsFactory

--- src/main/java/za/ac/cput/factory/team/TeamStatisticsFactory.java
+++ src/main/java/za/ac/cput/factory/team/TeamStatisticsFactory.java
@@ -4,15 +4,17 @@
  * @author Mike Somelezo Tyolani (220187568)
  * 04 April 2022
  */
-package za.ac.cput.factory;
+package za.ac.cput.factory.team;
 
-import za.ac.cput.domain.TeamStatistics;
+import za.ac.cput.domain.team.TeamStatistics;
 import za.ac.cput.util.Helper;
 
 public class TeamStatisticsFactory {
-    public static TeamStatistics createTeamStatistics(String trophiesWon, String matchesPlayed) {
+    public static TeamStatistics createTeamStatistics(String teamId, String trophiesWon, String matchesPlayed) {
+        Helper.checkStringParam("teamID", teamId);
+        Helper.checkStringParam("teamName", trophiesWon);
+        Helper.checkStringParam("numberOfPlayers", matchesPlayed);
 
-        String teamId = Helper.generateID();
         return new TeamStatistics.Builder().setTeamId(teamId)
                 .setTrophiesWon(trophiesWon)
                 .setMatchesPlayed(matchesPlayed)
