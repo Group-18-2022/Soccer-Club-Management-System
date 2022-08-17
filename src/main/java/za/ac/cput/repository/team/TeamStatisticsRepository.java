bef45b86c4e9358640f7b27fde72f5ed5bafb481
miketyo <mikesomelezotyolani41@gmail.com>
2022-08-17 12:26:00 +0200

    Updated TeamRepository and TeamStatisticsRepository

--- src/main/java/za/ac/cput/repository/team/TeamStatisticsRepository.java
+++ src/main/java/za/ac/cput/repository/team/TeamStatisticsRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.team.TeamStatistics;
 
+import java.util.Optional;
+
 @Repository
 public interface TeamStatisticsRepository extends JpaRepository<TeamStatistics, String> {
+    Optional<TeamStatistics> findTeamStatisticsByTeamId(String teamId);
 }
